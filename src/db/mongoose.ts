872269f73f6e1c9ce4c98256d22cf7b3fa41c5de
Mo Yasser <alalmy475@gmail.com>
2022-10-23 15:00:26 +0200

    updating database to switch between test & prod & dev

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -6,14 +6,15 @@ dotenv.config()
 const DB_USER = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
 const NODE_ENV = process.env.NODE_ENV
-let DB_HOST = null
-if (NODE_ENV === 'prod') {
-  DB_HOST = process.env.DB_HOST_PROD
+let DB_HOST = process.env.DB_HOST_PROD
+if (NODE_ENV === 'test') {
+  DB_HOST = process.env.DB_HOST_TEST
 } else if (NODE_ENV === 'dev') {
   DB_HOST = process.env.DB_HOST_DEV
 } else {
-  DB_HOST = process.env.DB_HOST_TEST
+  DB_HOST = process.env.DB_HOST_PROD
 }
+console.log(DB_HOST)
 const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.uz8vm7a.mongodb.net/${DB_HOST}?retryWrites=true&w=majority`
 
 const connectMongoDB = async () => {
